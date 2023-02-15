738f42928b31ebe33f50a80c1ebdfca0576a1bae
Nymy <nonno@kth.se>
2023-02-15 11:46:19 +0100

    Update UserTypes.js

--- src/Actions/UserActions/UserTypes.js
+++ src/Actions/UserActions/UserTypes.js
@@ -1,5 +1,9 @@
-export const EDIT_USER = "EDIT_USER";
-export const EDIT_USER_FAIL = "EDIT_USER_FAIL";
-export const GET_ALL_USER = "GET_ALL_USER";
-export const GET_ALL_USER_FAIL = "GET_ALL_USER_FAIL";
+export const USERSTATE = {
+    EDIT_USER: "EDIT_USER",
+    EDIT_USER_FAIL: "EDIT_USER_FAIL",
+    GET_ALL_USER: "GET_ALL_USER",
+    GET_ALL_USER_FAIL: "GET_ALL_USER_FAIL",
+};
+
+
 
